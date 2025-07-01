711292b75993bca6e4b19a4fe57a51109037351b
mahtabulsouravv <mahtabulsouravcse@gmail.com>
2025-07-01 22:55:13 +0600

    Navbar Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Navbar } from "../components/Navbar";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -12,8 +13,10 @@ export const Home = () => {
         <StarBackground />
 
         {/* Navbar */}
+        <Navbar />
 
         {/* Main Content 1.Hero 2.About 3.Skills 4.Project .. */}
+        {/* https://youtu.be/ifOJ0R5UQOc?t=4736 */}
 
         {/* Footer */}
 
