2eede12dcb1124f2d72204039d47f30e6bc4669d
mahtabulsouravv <mahtabulsouravcse@gmail.com>
2025-07-01 20:39:25 +0600

    ThemeToggle, StarBackground Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
+import { StarBackground } from "../components/StarBackground";
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
-    return <div>
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
         {/* Theme Toggle */}
+        <ThemeToggle />
 
         {/* Background Effects */}
+        <StarBackground />
 
-    </div>;
+        {/* Navbar */}
+
+        {/* Main Content 1.Hero 2.About 3.Skills 4.Project .. */}
+
+        {/* Footer */}
+
+    </div>
+    );
 }
